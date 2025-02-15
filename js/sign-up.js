031f743d3378cbeea57586b88c5b7fbcbf140153
katlehomadaba <katlehomadaba44@gmail.com>
2025-02-15 16:09:34 +0200

    Able to add information into the local storage

--- js/sign-up.js
+++ js/sign-up.js
@@ -6,13 +6,27 @@ const confirmPassword = document.getElementById('confirmPassword');
 const error_messages=document.getElementById('error_messages')
 
 form.addEventListener('submit',(e)=>{
+    e.preventDefault()//Prevents form from auto submitting 
     let errors=[];
     if(password.value !== confirmPassword.value){
-        e.preventDefault()
+        e.preventDefault()//Prevents form from auto submitting 
         errors.push('Password word do not match')
     }
     if(errors.length > 0){
         error_messages.innerHTML=errors.join('<br>')
+    }else{
+        let informationObject={
+            email: email.value,
+            username:username.value,
+            password:password.value,
+            confirmPassword:confirmPassword.value,
+        }
+        let  stringInformation=JSON.stringify(informationObject);
+                console.log(stringInformation);
+               
+
+        localStorage.setItem("userInformation",stringInformation);
+        console.log(localStorage);
     }
     
 })
