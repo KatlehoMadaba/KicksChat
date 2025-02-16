e00a657c6b513a83487c3179f197b968f4d3f7a7
katlehomadaba <katlehomadaba44@gmail.com>
2025-02-16 21:55:25 +0200

    Message sendable

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,3 @@
-
 window.onload = function() {
 //retriving email address from session
   const emailAddress=sessionStorage.getItem('email')
@@ -11,4 +10,28 @@ window.onload = function() {
   //displaying username in
   const showUsername=document.getElementById('showUsername');
   showUsername.textContent=domUsername;
+
+
+  // const textRecieved=document.getElementById('textRecieved');
+  // //retriving value stored in local storage
+  // textSent.localStorage.getItem("value")
+  
+  // sendBtn.addEventListener('submit',(e)=>{
+    //   e.preventDefault();
+    //   localStorage.setItem("value",chatContainer.value);
+    //   textSent.textContent=localStorage.getItem("value")
+    // })
+    const sendBtn= document.getElementById('sendBtn');
+    const chatContainer=document.getElementById('chatContainer');
+    const messageInput=document.getElementById('messageInput');
+    const textSent=document.getElementById('textSent');
+
+sendBtn.addEventListener('click',(e)=>{
+  e.preventDefault();
+  const messageValue=messageInput.value;
+  localStorage.setItem("message",messageValue)
+  console.log("Message saved to localSorage:",messageValue)
+  textSent.textContent=messageValue
+})
+
 };
