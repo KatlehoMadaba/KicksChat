7c3c5f111cd30118ffc43825b6778143a5b47356
katlehomadaba <katlehomadaba44@gmail.com>
2025-02-17 15:08:22 +0200

    creating variable current chatting users

--- js/chat.js
+++ js/chat.js
@@ -31,7 +31,8 @@ window.onload = function () {
     let ul = document.createElement("ul");
     let li = document.createElement("li");
     li.addEventListener('click',(e)=>{
-      loadChat(contact.username);
+      convoClick(contact.username)
+      sendMessage(contact.username);
     })
     let span = document.createElement("span");
     let span1 = document.createElement("span");
@@ -39,7 +40,6 @@ window.onload = function () {
     span1.classList.add("iconContact");
     span.classList.add("contactName");
     span.textContent = contact.username;
-    // span2.classList.add("contactTime");
     ul.appendChild(li);
     li.appendChild(span1);
     li.appendChild(span); //what ever is inside the bracket is the child of li
@@ -47,56 +47,60 @@ window.onload = function () {
   });
 
   //clicking on the user 
-  function loadChat(username){
+  function convoClick(username){
     document.addEventListener('click',(e)=>{
       convoConvo.textContent=username;
     })
   }
 
   //Sending a message
-  sendBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const messageValue = messageInput.value;
-    const dateTime = new Date().toString();
-    console.log(
-      "Message saved to localSorage:",
-      messageValue + "By this email address" + emailAddress
-    );
-    let messages = {
-      email: emailAddress,
-      username: domUsername,
-      message: messageInput.value,
-      date_time: dateTime,
-      status: "sent",
-    };
-    // //Json object to string
-    //messages to local storage
-    let storedMessages = JSON.parse(localStorage.getItem("messages")) || [];
-    storedMessages.innerHTML = "";
-    //displaying existing messages
-
-    storedMessages.forEach((mes) => {
-      let messageSent= document.createElement("div");
-      messageSent.classList.add("messageSent");
-      messageSent.textContent=mes.message
-      chatMessages.appendChild(messageSent)
-      const messageItem = document.getElementById("textSent");
-      if (messageItem !== null) {
-        messageItem.textContent = mes.message;
-      }
+  function sendMessage (recieverUsername) {
+     sendBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const messageValue = messageInput.value;
+      const dateTime = Date.now().toString();
+      console.log(
+        "Message saved to localSorage:",
+        messageValue + "By this email address" + emailAddress
+      );
+      let messages = {
+        email: emailAddress,
+        senderUsername: domUsername,
+        recieverUsername:recieverUsername,
+        message: messageInput.value,
+        date_time: dateTime,
+        status: "sent",
+      };
+      // //Json object to string
+      //messages to local storage
+      let storedMessages = JSON.parse(localStorage.getItem("messages")) || [];
+      storedMessages.innerHTML = "";
+      //displaying existing messages
+  
+      storedMessages.forEach((mes) => {
+        let messageSent= document.createElement("div");
+        messageSent.classList.add("messageSent");
+        messageSent.textContent=mes.message
+        chatMessages.appendChild(messageSent)
+        const messageItem = document.getElementById("textSent");
+        if (messageItem !== null) {
+          messageItem.textContent = mes.message;
+        }
+      });
+      storedMessages.push(messages);
+  
+      //saving the message to the array
+      localStorage.setItem("messages", JSON.stringify(storedMessages));
+      //getting last index of the arrray
+      const recentMessages = storedMessages[storedMessages.length - 1];
+      console.log("These are the recent messages" + recentMessages);
+      //display message
+      textSent.textContent = recentMessages.message;
+  
+      //clear text box after sending
+      messageInput.value = "";
+  
     });
-    storedMessages.push(messages);
+  }
 
-    //saving the message to the array
-    localStorage.setItem("messages", JSON.stringify(storedMessages));
-    //getting last index of the arrray
-    const recentMessages = storedMessages[storedMessages.length - 1];
-    console.log("These are the recent messages" + recentMessages);
-    //display message
-    textSent.textContent = recentMessages.message;
-
-    //clear text box after sending
-    messageInput.value = "";
-
-  });
 };
